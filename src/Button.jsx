9f21f92d4ce3338df38e8906c9ec40b5e24546d2
shivani charan <sushikim2050@gmail.com>
2024-08-15 21:37:34 +0530

    fix button

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,11 +2,10 @@ const Button = ({ handleBtnClick }) => {
   return (
     <div
       className="h-[60px] w-[60px] rounded-full bg-NeonGreen flex items-center justify-center z-20 fixed 
-sm:bottom-[12.25rem]
-base:bottom-[4.5rem]
-md:bottom-[8rem]
-xl:bottom-[7.85rem]
-2xl:bottom-[8rem]
+      translate-y-[32px]
+sm:top-[24rem]
+xl:top-[26rem]
+2xl:top-[28rem]
 cursor-pointer
 transition-shadow linear duration-300 
 shade
